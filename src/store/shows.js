1de29a1d5ea3b0c51e4acdc283267f23d089a220
Surajit Maity <surajitmaity12345@gmail.com>
2026-04-10 01:16:44 +0530

    added store setup

---
+++ src/store/shows.js
@@ -0,0 +1,13 @@
+import { defineStore } from 'pinia';
+
+export const useShowsStore = defineStore('shows', {
+  state: () => ({
+    shows: [],
+  }),
+  getters: {
+    
+  },
+  actions: {
+    
+  },
+});
