829a57665ff1967ace4b5f4730dfbe2570a0404e
Surajit Maity <surajitmaity12345@gmail.com>
2026-04-10 23:04:22 +0530

    added page not found component

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createWebHistory, createRouter } from 'vue-router';
 import Home from '@/views/Home.vue';
 import ShowDetails from '@/views/ShowDetails.vue';
 import SearchPage from '@/views/SearchPage.vue';
+import PageNotFound from '@/views/PageNotFound.vue';
 
 const routes = [
   { path: '/', component: Home },
   { path: '/show-details/:id', component: ShowDetails },
   { path: '/search/:searchText?', component: SearchPage },
+  { path: '/:pathMatch(.*)*', component: PageNotFound },
 ]
 
 export const router = createRouter({
---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="not-found-container">
+    <div class="content">
+      <h1 class="error-code">404</h1>
+      <h2 class="error-message">Page not found!</h2>
+      <RouterLink to="/">
+        <AppButton btnText="Back to Home" />
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import AppButton from '@/components/AppButton.vue';
+</script>
+
+<style scoped>
+.not-found-container {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  background-color: var(--clr-neutral-800);
+  color: var(--clr-neutral-200);
+  padding: 2rem;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.error-code {
+  font-size: 10rem;
+  font-weight: 800;
+  margin: 0;
+  color: var(--clr-neutral-200);
+}
+
+.error-message {
+  font-size: 2rem;
+  margin-block: 1rem;
+}
+</style>
