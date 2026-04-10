c34068a6d573d5d6e0953d788b51905552a465bf
Surajit Maity <surajitmaity12345@gmail.com>
2026-04-10 13:14:10 +0530

    updated header with better styling

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,70 +1,85 @@
 <template>
-  <header class="app-header">
-    <div class="header-items-left">
-      <div class="logo">
-        <img :src="logo" alt="TubeTrack Logo">
+  <div class="header-container">
+    <header class="app-header">
+      <div class="header-items-left">
+        <div class="logo">
+          <img :src="logo" alt="TubeTrack Logo" />
+        </div>
+        <div>
+          <nav class="main-nav">
+            <ul>
+              <li>
+                <RouterLink to="/">
+                  <img :src="homeIcon" alt="" />
+                  <span>Home</span>
+                </RouterLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
-      <div class="">
-        <nav class="main-nav">
-          <ul>
-            <li>
-              <img :src="homeIcon" alt="">
-              <span>Home</span>
-            </li>
-          </ul>
-        </nav>
+      <div class="header-items-right">
+        <div class="search">
+          <img :src="searchIcon" alt="" />
+        </div>
+        <div class="profile-invoker">
+          <img :src="userIcon" alt="" />
+        </div>
       </div>
-    </div>
-    <div class="header-items-right">
-      <div class="search">
-        <img :src="searchIcon" alt="">
-        <input type="text">
-      </div>
-      <div class="profile-invoker">
-        <img :src="userIcon" alt="">
-      </div>
-    </div>
-  </header>
+    </header>
+  </div>
 </template>
 
 <script setup>
-import logo from '@/assets/icons/TubeTrack-Logo.jpg';
+import logo from '@/assets/icons/TubeTrack-Logo.png';
 import homeIcon from '@/assets/icons/home-icon.svg';
 import searchIcon from '@/assets/icons/search-icon.svg';
 import userIcon from '@/assets/icons/user-icon.svg';
 </script>
 
 <style scoped>
-.app-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  max-width: 1400px;
-  margin: auto;
-  padding: 1rem 2rem;
+.header-container {
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+  background: linear-gradient(to bottom, rgb(0, 0, 0), transparent);
 
-  .header-items-left, .header-items-right {
+  .app-header {
     display: flex;
+    justify-content: space-between;
     align-items: center;
-    gap: 2rem;
-  }
+    max-width: 1400px;
+    margin: 0 auto;
+    padding: 1rem 2rem;
+    width: 100%;
+    position: relative;
 
-  .main-nav {
-    ul li {
+    .header-items-left,
+    .header-items-right {
+      display: flex;
+      align-items: end;
+      gap: 2rem;
+    }
+
+    .main-nav ul li a {
+      display: flex;
+      align-items: end;
+      gap: .5rem;
+      color: var(--clr-neutral-200);
+    }
+
+    .logo {
+      width: 10rem;
+    }
+
+    .search {
       display: flex;
       align-items: center;
       gap: .5rem;
     }
-  }
-
-  .logo {
-    width: 10rem;
-  }
-
-  .search {
-    display:flex;
-    align-items: center;
-    gap: .5rem;
   }
 }
 </style>
