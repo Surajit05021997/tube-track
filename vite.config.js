6fad2d211c71cb01c43742c750cb690148e0c72d
Surajit Maity <surajitmaity12345@gmail.com>
2026-04-13 16:40:23 +0530

    code refactor

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -23,4 +22,4 @@ export default defineConfig({
       exclude: ['node_modules/', 'src/assets/'],
     },
   },
-})
+});
